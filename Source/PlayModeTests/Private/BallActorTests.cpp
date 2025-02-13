4913c18f3fd5f95b7fd13bed4e5d8b09be59722e
Andrew Fray <andrew.fray@gmail.com>
2025-02-13 17:27:48 +0000

    spawning a ball for an in-level actor test

--- Source/PlayModeTests/Private/BallActorTests.cpp
+++ Source/PlayModeTests/Private/BallActorTests.cpp
@@ -1,23 +1,40 @@
 #include "CQTest.h"
 #include "ObjectBuilder.h"
 #include "Components/MapTestSpawner.h"
+#include "Helpers/CQTestAssetHelper.h"
 
 TEST_CLASS(FCGBallTests, "Project.ActorTests.Ball")
 {
 	TUniquePtr<FMapTestSpawner> Spawner;
+	AActor* Block;
+	float StartZ;
 protected:
 	BEFORE_EACH()
 	{
 		Spawner = MakeUnique<FMapTestSpawner>("/Game/FirstPerson/Maps", "FirstPersonMap");
 		Spawner->AddWaitUntilLoadedCommand(TestRunner);
-	}
-	
-	TEST_METHOD(WhenPlayerTouchesSpikeTrap_ThenPlayerLifted)
-	{
 		TestCommandBuilder
 		    .StartWhen([this](){return nullptr != Spawner->FindFirstPlayerPawn(); }, FTimespan::FromSeconds(30))
 		    .Do([this]{
-				
+		    	const auto Player = Spawner->FindFirstPlayerPawn();
+				const auto BlockClass = CQTestAssetHelper::GetBlueprintClass("SM_ChamferCube3_Blueprint");
+		    	const FTransform NearPlayer(Player->GetActorLocation()
+		    		+ Player->GetActorForwardVector()*20
+		    		+ FVector::UpVector*20);
+		    	StartZ = NearPlayer.GetLocation().Z;
+		    	Block = &TObjectBuilder<AActor>(*Spawner, BlockClass)
+		    	    .Spawn(NearPlayer);
 			});
+	}
+	
+	TEST_METHOD(Physics_InAir_Falls)
+	{
+		TestCommandBuilder
+		    .Until([this]
+		    {
+			    const float FallAmount = StartZ - Block->GetActorLocation().Z;
+		    	return FallAmount > 10;
+		    },
+		    FTimespan::FromSeconds(3));
 	}
 };
